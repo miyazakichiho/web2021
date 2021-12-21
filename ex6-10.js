fe18de290fe5b71e18b84e3c001450208cb33468
miyazakichiho <zm18455@josai.ac.jp>
2021-12-21 02:07:38 +0000

    1221

--- ex6-10.js
+++ ex6-10.js
@@ -1,4 +1,5 @@
 function imgchange1() 
 {document.getElementById('logo').src= './image/image.gif';}
-function image2() 
-{document.getElementById('logo').src= './image/image.gif';}
+function imgchange2() 
+{document.getElementById('logo').src= './image/josai.png';}
+
