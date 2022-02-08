0c0201a494ae8c254846e92e3760d80dde9fd0e5
miyazakichiho <65930942+miyazakichiho@users.noreply.github.com>
2022-02-08 21:53:03 +0900

    Update ex6-11.js

--- ex6-11.js
+++ ex6-11.js
@@ -4,9 +4,9 @@ var str1 = "一文字ずつ表示します。";
 //テキストボックスの文字数
 var cnt1 = document.timer1.moji1.value.length; 
 //文字が全部表示されているか確認
-if ( cnt < 11 ) { 
+if ( cnt1 < 11 ) { 
 //現在より1文字多く切り出して表示
-document.timer1.moji1.value = str.substr(0,cnt+1); }
+document.timer1.moji1.value = str1.substr(0,cnt1+1); }
 else { 
 //全て表示されたら、空文字に戻す
 document.timer1.moji1.value= ""; }
