a88322b74ec3b695f32ae3b05e2042cd495ed86a
miyazakichiho <65930942+miyazakichiho@users.noreply.github.com>
2022-02-08 21:42:40 +0900

    Update ex6-11.js

--- ex6-11.js
+++ ex6-11.js
@@ -2,6 +2,21 @@ function hyoji() {
 //表示する文字
 var str = "一文字ずつ表示します。"; 
 //テキストボックスの文字数
+var cnt = document.timer1.moji1.value.length; 
+//文字が全部表示されているか確認
+if ( cnt < 11 ) { 
+//現在より1文字多く切り出して表示
+document.timer1.moji1.value = str.substr(0,cnt+1); }
+else { 
+//全て表示されたら、空文字に戻す
+document.timer1.moji1.value= ""; }
+//setTimeout()を含む関数を呼び出す
+setTimeout("hyoji()",1000); }
+
+function startfnc() { 
+//表示する文字
+var str = "一文字ずつ表示します。"; 
+//テキストボックスの文字数
 var cnt = document.timer.moji.value.length; 
 //文字が全部表示されているか確認
 if ( cnt < 11 ) { 
@@ -12,10 +27,5 @@ else {
 document.timer.moji.value= ""; }
 //setTimeout()を含む関数を呼び出す
 setTimeout("hyoji()",1000); }
-
-function startfnc() { 
-//表示する文字
-var str = "一文字ずつ表示します。"; 
-
 
 function mes() { alert("3秒経ちました！"); }
